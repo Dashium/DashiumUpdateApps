e1a02e07dc9b0e08e38012774837c1a5b28a5d19
Tai Studio <tai.studio@outlook.fr>
2021-12-28 20:06:34 +0100

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir('/DashiumOS', (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+
+        child_process.exec(`bash ${__dirname.replace(/\\/g, '/')}/updateALL.sh ${file}`, (err, stdout) => {
+            console.log(err, stdout);
+        });
+    });
+});
---
+++ updateALL.sh
@@ -0,0 +1,5 @@
+cd $1
+
+git stash
+git reset --merge
+git pull
